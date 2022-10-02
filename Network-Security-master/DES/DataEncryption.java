1db1bc9d9285662822efe3377fa1231d3da68b6f
Warmonger <86009160+Malwareman007@users.noreply.github.com>
2022-10-02 23:10:15 +0530

    commit

--- Network-Security-master/DES/DataEncryption.java
+++ Network-Security-master/DES/DataEncryption.java
@@ -1,7 +1,4 @@
 
-/*
- * @author: Mujtaba Ali (13p-6134)
- */
 public class DataEncryption extends Conversion{
 	public int []IP = {58,    50,   42,    34,    26,   18,    10,    2,
             60,    52,   44,    36,    28,   20,    12,    4,
